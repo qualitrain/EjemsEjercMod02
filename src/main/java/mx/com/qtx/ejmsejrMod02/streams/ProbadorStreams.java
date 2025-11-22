42e93ef9001c31fbbd73d36c2a5aa417faf24485
alejandroQtx <qualitrain@gmail.com>
2025-11-22 11:30:04 -0600

    Se agrega ejm Streams c/sorted

--- src/main/java/mx/com/qtx/ejmsejrMod02/streams/ProbadorStreams.java
+++ src/main/java/mx/com/qtx/ejmsejrMod02/streams/ProbadorStreams.java
@@ -9,13 +9,35 @@ public class ProbadorStreams {
             new Gato(500,"Félix","Angora",5.3),
             new Gato(3,"Demóstenes","Angora",7.7),
             new Gato(7,"Panza","Siamés",6.5),
+            new Gato(17,"Danilo","Criollo",6.2),
+            new Gato(70,"Hermilo","Birmano",6.4),
             new Gato(34,"Cucho","Burmés",9.2),
             new Gato(65,"Benito","Abisinio",10.3),
             new Gato(200,"Micifuz","Persa",8.1)
     );
     public static void main(String[] args) {
     //    test_BasicosDeStream();
-        record GatoFlaco(String nombre,Double peso){};
+     //   testStreamConMapRecord();
+          testSorted();
+
+    }
+
+    private static void testSorted() {
+        record GatoFlaco(String nombre,Double peso){}
+        ;
+        List<Gato> lstGatos1 = new ArrayList<>(lstMininos);
+
+        List<GatoFlaco> lstGatosFlacos = lstGatos1.stream()
+                .filter(gatoI -> gatoI.getPesoEnKg() < 7)
+                .map(gatoI -> new GatoFlaco(gatoI.getNombre(), gatoI.getPesoEnKg()))
+                .sorted((gf1,gf2)-> (gf1.peso() < gf2.peso ? -1 : 1))
+                .toList();
+        System.out.println("lstGatosFlacos = " + lstGatosFlacos);
+    }
+
+    private static void testStreamConMapRecord() {
+        record GatoFlaco(String nombre,Double peso){}
+        ;
         List<Gato> lstGatos1 = new ArrayList<>(lstMininos);
 
         List<GatoFlaco> lstGatosFlacos = lstGatos1.stream()
@@ -29,7 +51,6 @@ public class ProbadorStreams {
                 .map(gatoI -> new GatoFlaco(gatoI.getNombre(), gatoI.getPesoEnKg()));
         int nGatosFlacos = (int) streamGatosFlacos.count();
         System.out.println("\nnGatosFlacos = " + nGatosFlacos);
-
     }
 
     private static void test_BasicosDeStream() {
